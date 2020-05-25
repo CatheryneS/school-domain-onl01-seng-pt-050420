580ecc65a749302fffd149707ed30523e4e86bf9
Catheryne Small <catheryn.small@gmail.com>
2020-05-25 02:01:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_reader :school, :roster
+  
+  def initialize(school)
+    @school = school 
+    @roster = {}
+  end
+    
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+  
+  def grade(grade_students)
+    @roster[grade_students]
+  end
+  
+  def sort 
+    @roster.sort
+  end
+end
